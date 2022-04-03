ae0da1df4b4c7ff8b260446be55463c15b331a31
Jared Ruiz <jared707@gmail.com>
2022-04-02 23:07:11 -0500

    Added first portion of functionality to index.js. Created some functions inside generateMarkdown as well.

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,40 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+       type: `input`,
+       name: `title`,
+       message: `What is the title of your project?` 
+    },
+    {
+        type: `input`,
+        name: `description`,
+        message: `Please provide a description for your project.`
+    },
+    {
+        type: `list`,
+        name: `license`,
+        message: `Please select a license.`,
+        choices: [`MIT`, `IBM`, `GNU`, `None`]
+    }
+
+
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        const template = generateMarkdown(answers);
+        console.log(template);
+    })
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,46 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === `None`) {
+    return '';
+  }
+  else {
+    return `[![License: ${license}](https://img.shields.io/badge/License-${license}-yellow.svg)](https://opensource.org/licenses/${license})`
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === `None`) {
+    return '';
+  }
+  else {
+    return `[Link to ${license} license]: https://opensource.org/licenses/${license}`;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === `None`) {
+    return '';
+  }
+  else {
+    return `
+    ## License Information
+    ${renderLicenseBadge(data.license)}
+    ${renderLicenseLink(data.license)}
+
+  `
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+    ## ${data.description}
+    ${renderLicenseSection}
 `;
 }
 
